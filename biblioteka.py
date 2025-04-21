847e6d8c3898e5f5a7939d8f315977aa4be7e163
mars-25 <marswacht@gmail.com>
2025-04-21 18:55:31 +0200

    Dodanie bezposredniego uruchomienia z konsoli if_name=_main...

--- biblioteka.py
+++ biblioteka.py
@@ -44,30 +44,31 @@ class Seriale(BibliotekaFilmow):
     def get_series(self):
         return(Seriale)
     
+if __name__ == "__main__":
 
-new_film = [
-    BibliotekaFilmow("Batman", 2021, "triller"),
-    BibliotekaFilmow("Sumatra", 2000, "sf"),
-    Seriale("Simpsonowie", 1990, "kreskowka", 1, 2),
-    Seriale("Moda na sukces", 1900, "popularno-naukowy", 89, 32),
-    BibliotekaFilmow("Solaris", 2028, "komedia"),
-    Seriale("Miś Koralgol", 2021, "kreskowka", 1, 1),
-]
+    new_film = [
+        BibliotekaFilmow("Batman", 2021, "triller"),
+        BibliotekaFilmow("Sumatra", 2000, "sf"),
+        Seriale("Simpsonowie", 1990, "kreskowka", 1, 2),
+        Seriale("Moda na sukces", 1900, "popularno-naukowy", 89, 32),
+        BibliotekaFilmow("Solaris", 2028, "komedia"),
+        Seriale("Miś Koralgol", 2021, "kreskowka", 1, 1),
+    ]
 
-new_film[0].play()
-new_film[1].play()
-new_film[0].play()
-new_film[1].play()
+    new_film[0].play()
+    new_film[1].play()
+    new_film[0].play()
+    new_film[1].play()
 
-with open("biblioteka.txt",'w') as write_file:
-    write_file.write("lista filmow z rokiem produkcji:\n")
-    for film in new_film:
-        write_file.write(f"- {film.tytul} {film.rok} {film.gatunek} {film.liczba_odtworzen}\n")
+    with open("biblioteka.txt",'w') as write_file:
+        write_file.write("lista filmow z rokiem produkcji:\n")
+        for film in new_film:
+            write_file.write(f"- {film.tytul} {film.rok} {film.gatunek} {film.liczba_odtworzen}\n")
 
-#tylko do sprawdzania 
-with open("biblioteka.txt", 'r') as read_file:
-    for line in read_file.read().splitlines():
-        print(line)
+    #tylko do sprawdzania 
+    with open("biblioteka.txt", 'r') as read_file:
+        for line in read_file.read().splitlines():
+            print(line)
 
-print(f"Wyświetlanie filmu:\n {new_film[1]}")
-print(f"Wyświetlanie info o serialu:\n {new_film[2]}")
+    print(f"Wyświetlanie filmu:\n {new_film[1]}")
+    print(f"Wyświetlanie info o serialu:\n {new_film[2]}")
